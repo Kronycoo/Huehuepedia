012518054a8d10574950e7ea4601289d1d5de605
Alberto Gutierrez <88956786+Kronycoo@users.noreply.github.com>
2022-02-09 17:04:57 -0600

    Add files via upload

---
+++ Huehuepedia/JS/menu.js
@@ -0,0 +1,8 @@
+(function () {
+    const button = document.getElementById('btn-menu');
+    button.addEventListener('click', menu);
+
+    function menu() {
+        document.getElementById('nav-menu').classList.toggle('aparecer-menu');
+    }
+})();
